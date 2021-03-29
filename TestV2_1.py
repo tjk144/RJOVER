94189006ed4dd0669b28194df3c8e94eb7f25604
tjk144 <tjk144@case.edu>
2021-03-29 14:38:49 -0400

    updated documentation to include remote control

--- TestV2_1.py
+++ TestV2_1.py
@@ -28,6 +28,7 @@ import sip
 from gnuradio import analog
 from gnuradio import audio
 from gnuradio import blocks
+from gnuradio import filter
 from gnuradio import gr
 import sys
 import signal
@@ -40,7 +41,7 @@ import time
 
 from gnuradio import qtgui
 
-class TestV2_0(gr.top_block, Qt.QWidget):
+class TestV2_1(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "RTL-SDR reciever")
@@ -63,7 +64,7 @@ class TestV2_0(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "TestV2_0")
+        self.settings = Qt.QSettings("GNU Radio", "TestV2_1")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -77,8 +78,8 @@ class TestV2_0(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.volume = volume = 50e-3
-        self.tuning = tuning = 1.267e6
-        self.samp_rate = samp_rate = 384000
+        self.tuning = tuning = 0.88e6
+        self.samp_rate = samp_rate = 1536000
 
         ##################################################
         # Blocks
@@ -86,7 +87,7 @@ class TestV2_0(gr.top_block, Qt.QWidget):
         self._volume_range = Range(0, 10, 50e-3, 50e-3, 200)
         self._volume_win = RangeWidget(self._volume_range, self.set_volume, 'volume', "counter_slider", float)
         self.top_grid_layout.addWidget(self._volume_win)
-        self._tuning_range = Range(0.5e6, 30e6, 10e3, 1.267e6, 200)
+        self._tuning_range = Range(0.5e6, 30e6, 10e3, 0.88e6, 200)
         self._tuning_win = RangeWidget(self._tuning_range, self.set_tuning, 'tuning', "counter_slider", float)
         self.top_grid_layout.addWidget(self._tuning_win)
         self.rtlsdr_source_0 = osmosdr.source(
@@ -104,8 +105,8 @@ class TestV2_0(gr.top_block, Qt.QWidget):
         self.rtlsdr_source_0.set_bb_gain(20, 0)
         self.rtlsdr_source_0.set_antenna('', 0)
         self.rtlsdr_source_0.set_bandwidth(200000, 0)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
-            1024, #size
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+            2048, #size
             firdes.WIN_BLACKMAN_hARRIS, #wintype
             tuning, #fc
             200e3, #bw
@@ -123,7 +124,6 @@ class TestV2_0(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.enable_control_panel(False)
 
 
-        self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
 
         labels = ['', '', '', '', '',
             '', '', '', '', '']
@@ -145,28 +145,41 @@ class TestV2_0(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(
+            32,
+            firdes.low_pass(
+                1,
+                samp_rate,
+                samp_rate/64,
+                12000,
+                firdes.WIN_HAMMING,
+                6.76))
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(volume)
         self.audio_sink_0 = audio.sink(48000, '', True)
         self.analog_am_demod_cf_0 = analog.am_demod_cf(
-        	channel_rate=samp_rate,
-        	audio_decim=8,
-        	audio_pass=5000,
-        	audio_stop=10000,
+        	channel_rate=48000,
+        	audio_decim=1,
+        	audio_pass=1000,
+        	audio_stop=8000,
         )
+        self.analog_agc_xx_0 = analog.agc_cc(6.25e-4, 1.0, 1.0)
+        self.analog_agc_xx_0.set_max_gain(65536)
 
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_agc_xx_0, 0), (self.analog_am_demod_cf_0, 0))
         self.connect((self.analog_am_demod_cf_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.analog_am_demod_cf_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.rtlsdr_source_0, 0), (self.analog_am_demod_cf_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_agc_xx_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "TestV2_0")
+        self.settings = Qt.QSettings("GNU Radio", "TestV2_1")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
 
@@ -190,13 +203,14 @@ class TestV2_0(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.samp_rate/64, 12000, firdes.WIN_HAMMING, 6.76))
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
 
 
 
 
 
-def main(top_block_cls=TestV2_0, options=None):
+def main(top_block_cls=TestV2_1, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
