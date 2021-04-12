b9ccaa1526e8dc46b33c9844aea020ec400a1cfa
tjk144 <tjk144@case.edu>
2021-04-12 14:20:56 -0400

    Updating the job.sh to have gui and fixing TestV5.grc to be correct

--- job.sh
+++ job.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+echo "Please choose the filename for the python script that will be used for the data collection. Make sure that this script has already been ran through editor.sh to make sure that the looping code has been implemented properly."
+file=$(zenity --file-selection --confirm-overwrite)
+echo "Your file you will be using for collection is: $file"
 echo "Please create/choose a .wav file in the file selection tool. By default the date and time will be placed before any name that you choose." 
-filename=$(zenity --file-selection --save --confirm-overwrite);echo "Your filepath is: $filename"
+filename=$(zenity --file-selection --save --confirm-overwrite)
+echo "Your filepath is: $filename"
 flag="False"
 removedate=$(zenity --entry --width=400 --title="Please enter 0 if no dates will be prefixed." --text="Enter a number to continue" )
 if [ $removedate != "0" ]
@@ -11,11 +15,11 @@ else
 	flag="True"
 fi
 
-days=$(zenity --entry --width=400 --title="How many days would you like to run for?" --text="Please insert an integer atleast 1")
+days=$(zenity --entry --width=400 --title="How many half-days would you like to run for?" --text="Please insert an integer atleast 1")
 
 for i in $(seq 1 1 $( printf "%.0f" $days )) 
 do
-	timeout 1m python3 TestV5.py "${filename}" "${flag}"
+	timeout 20s python3 $file "${filename}" "${flag}"
 	echo "finished $i time at $(date)" 
 	echo " "
 done
